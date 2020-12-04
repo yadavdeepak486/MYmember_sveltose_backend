3dbd98a393f0bdb4e8f4f4e1764ebed297e9a6ac
tekeshwar810 <tekeshwar810@gmail.com>
2020-12-04 15:39:34 +0530

    4dec

--- controllers/addmember.js
+++ controllers/addmember.js
@@ -18,3 +18,74 @@ exports.addmember = (req, res) => {
     }) 
    
 }
+
+exports.memberinfo = (req,res)=>{
+    console.log('id',req.params)
+    var memberID = req.params.memberID;
+    console.log(memberID) 
+    addmemberModal.findById(memberID).exec((err,data)=>{
+        if(err){
+            res.send({error:'member is not found'})
+        }
+        else{
+            res.send(data)
+        }
+    })
+}
+
+exports.deletemember = (req,res)=>{
+    console.log('id',req.params)
+    var memberID = req.params.memberID;
+    addmemberModal.findByIdAndDelete(memberID).exec((err,data)=>{
+        if(err){
+            res.send({error:'member is not delete'})
+        }
+        else{
+            res.send({msg:'member is delete'})
+        }
+    })
+}
+
+exports.updatemember = (req,res)=>{
+    console.log('id',req.params)
+    var memberID = req.params.memberID;
+    var data = req.body
+  addmemberModal.findByIdAndUpdate(memberID,{
+  
+  studentType:data.studentType,
+  firstName:data.firstName,
+  lastName:data.lastName,
+  dob:data.dob,
+  age:data.age,
+  gender:data.gender,
+  email:data.email,
+  primaryPhone:data.primaryPhone,
+  secondaryNumber:data.secondaryNumber,
+  address:data.address,
+  country:data.country,
+  state:data.state,
+  zipPostalCode:data.zipPostalCode,
+  studentBeltSize:data.studentBeltSize,
+  program:data.program,
+  startDate:data.startDate,
+  expiredDate:data.expiredDate,
+  lastPromotion:data.lastPromotion,
+  location:data.location,
+  ID:data.ID,
+  dan:data.dan,
+  customId:data.customId,
+  leadsTracking:data.leadsTracking,
+  staff:data.staff,
+  intrested:data.intrested,
+  school:data.schoool,
+  addToGroup:data.addToGroup,
+  familyName:data.familyName
+    }).exec((err,data)=>{
+        if(err){
+            res.send({error:'member is not delete'})
+        }
+        else{
+            res.send({msg:'member is update'})
+        }
+    })
+}
--- routes/addmember.js
+++ routes/addmember.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { addmember }= require("../controllers/addmember")
+const { addmember,
+        memberinfo,
+        deletemember,
+        updatemember
+      }= require("../controllers/addmember")
+const { requireSignin } = require('../controllers/auth');
 
-router.post('/addmember',addmember) 
+router.get('/member/memberinfo/:userID/:memberID',requireSignin,memberinfo)
 
+router.post('/member/addmember/:userID',requireSignin,addmember) 
+
+router.delete('/member/deletemember/:userID/:memberID',requireSignin,deletemember)
+
+router.put('/member/updatemember/:userID/:memberID',requireSignin,updatemember)
 module.exports = router
